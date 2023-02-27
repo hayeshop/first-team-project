645e15c24f9eb93e87b9fcd49ae265763788c61b
HaKong <109659081+xxxjoyxxx@users.noreply.github.com>
2023-02-27 17:40:52 +0900

    Add files via upload

--- src/main/java/kr/co/mall/mapper/AdminMapper.java
+++ src/main/java/kr/co/mall/mapper/AdminMapper.java
@@ -1,5 +1,9 @@
 package kr.co.mall.mapper;
 
+import kr.co.mall.vo.AdminVo;
+
 public interface AdminMapper {
 
+	public AdminVo adminlogin(AdminVo avo);
+	
 }
--- src/main/java/kr/co/mall/mapper/LoginMapper.java
+++ src/main/java/kr/co/mall/mapper/LoginMapper.java
@@ -1,5 +1,9 @@
 package kr.co.mall.mapper;
 
-public interface LoginMapper {
+import kr.co.mall.vo.MemberVo;
 
+public interface LoginMapper {
+    public MemberVo login_ok(MemberVo mvo);
+    public String userid_search(String name,String email);
+    public String pwd_search(String name,String email,String userid);
 }
