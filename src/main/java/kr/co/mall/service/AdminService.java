087df86c1b03478debed32702f382756e0009559
HaKong <109659081+xxxjoyxxx@users.noreply.github.com>
2023-02-27 17:41:13 +0900

    Add files via upload

--- src/main/java/kr/co/mall/service/AdminService.java
+++ src/main/java/kr/co/mall/service/AdminService.java
@@ -1,5 +1,13 @@
 package kr.co.mall.service;
 
+import javax.servlet.http.HttpSession;
+
+import kr.co.mall.vo.AdminVo;
+
 public interface AdminService {
+
+	public String adminmain(HttpSession session);
+
+	public String adminlogin_ok(AdminVo avo, HttpSession session);
 
 }
--- src/main/java/kr/co/mall/service/AdminServiceImpl.java
+++ src/main/java/kr/co/mall/service/AdminServiceImpl.java
@@ -1,5 +1,48 @@
 package kr.co.mall.service;
 
-public class AdminServiceImpl {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import kr.co.mall.mapper.AdminMapper;
+import kr.co.mall.vo.AdminVo;
+
+@Service
+@Qualifier("as")
+public class AdminServiceImpl implements AdminService {
+
+	@Autowired
+	private AdminMapper mapper;
+
+	@Override
+	public String adminmain(HttpSession session) {
+		if(session.getAttribute("userid")==null)
+			return "/admin/adminlogin";
+		if(session.getAttribute("userid").equals("admin"))
+		{
+			
+			return "/admin/adminmain";
+		}
+		else
+		{
+			return "redirect:/main/main";
+		}
+	}
+
+	@Override
+	public String adminlogin_ok(AdminVo avo,HttpSession session) {
+		AdminVo avo2=mapper.adminlogin(avo);
+		if(avo2==null)
+		{
+			return "redirect:/main/main";			
+		}
+		else
+		{
+			session.setAttribute("userid", avo2.getUserid());
+			return "redirect:/admin/adminmain";
+		}
+	}
 }
