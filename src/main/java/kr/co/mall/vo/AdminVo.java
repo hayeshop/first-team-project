0273aae842b8772e61348316cfb4dd2c04d742a8
HaKong <109659081+xxxjoyxxx@users.noreply.github.com>
2023-02-27 17:41:34 +0900

    Add files via upload

---
+++ src/main/java/kr/co/mall/vo/AdminVo.java
@@ -0,0 +1,22 @@
+package kr.co.mall.vo;
+
+public class AdminVo {
+
+	private String userid,pwd;
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+}
