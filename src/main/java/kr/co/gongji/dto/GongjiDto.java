44c100cef3bcdd532432bfa264ccef1d2e866726
12yeju <124245722+12yeju@users.noreply.github.com>
2023-02-28 17:18:38 +0900

    Create GongjiDto.java

---
+++ src/main/java/kr/co/gongji/dto/GongjiDto.java
@@ -0,0 +1,39 @@
+package kr.co.gongji.dto;
+
+public class GongjiDto {
+	
+	private int id, chk;
+	private String title, content, writeday;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getChk() {
+		return chk;
+	}
+	public void setChk(int chk) {
+		this.chk = chk;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriteday() {
+		return writeday;
+	}
+	public void setWriteday(String writeday) {
+		this.writeday = writeday;
+	}
+	
+}
